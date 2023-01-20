43d3bda1493f18db81409810b7f3d3f82b9993b6
ilia gazdaliev <ilia.gazdaliev@maxiru.com>
2023-01-20 20:07:21 +0100

    stable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,28 @@
 const papercups = require('@papercups-io/papercups')('SFMyNTY.g2gDbAAAAAJoAmQAB3VzZXJfaWRiAAAX82gCZAAKYWNjb3VudF9pZG0AAAAkMjNiOTI5YmYtMTMzOS00N2M3LTljYWMtZTYwNGM2ZTVhMTEwam4GANII9cqFAWIAAVGA.LkxmLEj2ru7MgjohZHe9N-TKvHsOuCn92BGp-JfNF74');
-
 const cors = require('cors');
-
 const express = require('express')
+const { dockStart } = require('@nlpjs/basic');
+
+let nlp
+(async () => {
+  const dock = await dockStart({ use: ['Basic']});
+  nlp = dock.get('nlp');
+  nlp.addLanguage('en');
+
+  nlp.addDocument('en', 'sweepstakes dont work', 'sweepstakes')
+  nlp.addDocument('en', 'missing sweepstakes prize', 'sweepstakes')
+  nlp.addDocument('en', 'how does sweepstake work ?', 'sweepstakes')
+  nlp.addDocument('en', 'cant enter sweepstakes', 'sweepstakes')
+
+  nlp.addDocument('en', 'how can I swap tokens ?', 'swaps')
+  nlp.addDocument('en', 'swap dont work', 'swaps')
+  nlp.addDocument('en', 'how to I transfer my tokens ', 'swaps')
+
+
+  nlp.addAnswer('en', 'sweepstakes', 'Sweepstakes answer is...');
+  nlp.addAnswer('en', 'swaps', 'Swaps answer is...');
+  await nlp.train();
+})();
 
 const app = express()
 
@@ -10,8 +30,6 @@ app.use(cors());
 app.use(express.json());
 
 const port = 3000
-
-
   export interface Metadata {
     appVersion: string;
     codepush: string;
@@ -72,11 +90,15 @@ app.post('/', async(req, res) => {
     case 'message:created':
       const data = payload as Message;
       if(data.customer_id) {
-        await papercups.messages
-          .create({
-            body: 'Hello world!',
-            conversation_id: data.conversation_id,
-          })
+        const response = await nlp.process('en', data.body);
+        console.log(response.answer);
+        if(response.answer) {
+          await papercups.messages
+            .create({
+              body: response.answer,
+              conversation_id: data.conversation_id,
+            })
+        }
       }
       break
     case 'conversation:created':
